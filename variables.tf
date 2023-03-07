b12a00d114489f253a0c595b01e7e006ae5a854b
bogdan <bohdan.devops@alpteam.one>
2023-03-07 20:41:09 +0200

    update

--- variables.tf
+++ variables.tf
@@ -4,13 +4,13 @@
 variable  "aws_access_key" {
   type        = string
   description = "Add AWS access key for terraform"
-  default     = "XXXXXXXXXXXXXXXXXXXX"
+#  default     = ""
 }
 
 variable  "aws_secret_key" {
   type        = string
   description = "Add AWS secret key for terraform"
-  default     = "XXXXXXXXXXXXXXXXXXXX"
+#  default     = ""
 }
 
 variable  "aws_region" {
@@ -31,7 +31,7 @@ variable  "project_name" {
 
 variable  "vpc_cidr_block" {
   type        = string
-  description = "Set CIDR block for VPC "
+  description = "Set CIDR block for VPC"
   default     = "10.10.0.0/16"
 }
 
@@ -52,7 +52,7 @@ variable  "subnet_2_cidr_block" {
 variable "admin_ips" {
   type        = list
   description = "Add admin IPs for icmp/ssh ingress rules.\nExapmle:\n[\"x.x.x.x/32\",\"y.y.y.y/24\",\"0.0.0.0/0\"]"
-  default     = []
+#  default     = []
 }
 
 variable "allow_ports" {
@@ -71,12 +71,12 @@ variable "allow_ips" {
 
 variable "ec2_instance_type" {
   type        = string
-  description = "EC2 instance type"
+  description = "Set EC2 instance type"
   default     = "t2.micro"
 }
 
 variable "ssh_public_key" {
   type        = string
-  description = "Public SSH key"
-  default     = "XXXXXXXXXXXXXXXXXXXX"
+  description = "Add public SSH key for EC2"
+#  default     = ""
 }
